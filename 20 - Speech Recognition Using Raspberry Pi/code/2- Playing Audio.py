bf9ad9936543371ed66e1407625d6f4b499fec24
Aslm Fawzy Abd-Elkader <78029611+Aslm-Fawzy@users.noreply.github.com>
2022-08-04 21:30:51 +0200

    Delete 20 - Speech Recognition Using Raspberry Pi directory

--- 20 - Speech Recognition Using Raspberry Pi/code/2- Playing Audio.py
+++
@@ -1,7 +0,0 @@
-import pygame
-#initialize the mixer module
-pygame.mixer.init()
-#Load a music file for playback
-pygame.mixer.music.load("../Records/date.mp3") # can be .wav
-#Start the playback of the music stream
-pygame.mixer.music.play()
